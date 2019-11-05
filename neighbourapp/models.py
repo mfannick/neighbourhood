67289ce130b78368c2034a9cc24d43884393008b
mfannick <mfannick1@gmail.com>
2019-11-05 21:02:23 +0200

    database structure

--- neighbourapp/models.py
+++ neighbourapp/models.py
@@ -6,9 +6,9 @@ from django.contrib.auth.models import User
 class Neighbour(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
     # neighbourOccupantsCount=models.IntegerField()
-    neighbourNamChoices=(('Kimironko','Kimironko'),
-                   ('Nyamirambo','Nyamirambo'),
-                   ('Rebero','Rebero'),
+    neighbourNamChoices=(('Amahoro','Amahoro'),
+                   ('Agaciro','Agaciro'),
+                   ('Intsinzi','Intsinzi'),
 
 
     )
@@ -28,16 +28,36 @@ class Neighbour(models.Model):
 class Business(models.Model):
     businessName=models.CharField(max_length=30)
     businessEmail=models.EmailField()
+    businessImage=models.ImageField(upload_to='business/')
+    neighbourNamChoices=(('Amahoro','Amahoro'),
+                   ('Agaciro','Agaciro'),
+                   ('Intsinzi','Intsinzi'),
+
+
+    )
+    businessLocation=models.CharField(max_length=60,choices=neighbourNamChoices)
     neighbourF=models.ForeignKey(Neighbour,on_delete=models.CASCADE)
 
     def __str__(self):
         return self.businessName
+    @classmethod
+    def searchBusiness(cls,search_term):
+        business = cls.objects.filter(businessName__icontains=search_term)
+        return business
 
 
 class Police(models.Model):
     policeStationName=models.CharField(max_length=30)
     policeEmergencyNbr=models.IntegerField()
+    neighbourNamChoices=(('Amahoro','Amahoro'),
+                   ('Agaciro','Agaciro'),
+                   ('Intsinzi','Intsinzi'),
+
+
+    )
+    policeLocation=models.CharField(max_length=60,choices=neighbourNamChoices)
     neighbourF=models.ForeignKey(Neighbour,on_delete=models.CASCADE)
+    policeImage=models.ImageField(upload_to='police/')
 
     def __str__(self):
         return self.policeStationName
@@ -59,6 +79,14 @@ class Post(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     postName=models.CharField(max_length=30)
     postText=models.TextField()
+    postImage=models.ImageField(default='default.jpg',upload_to='post/')
+    neighbourNamChoices=(('Amahoro','Amahoro'),
+                   ('Agaciro','Agaciro'),
+                   ('Intsinzi','Intsinzi'),
+
+
+    )
+    postNeighbourName=models.CharField(max_length=60,choices=neighbourNamChoices)
     
 
 
